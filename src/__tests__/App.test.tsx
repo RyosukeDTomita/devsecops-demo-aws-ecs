0c55f553f32f0d2babbdfba606e700ee10960b37
RyosukeDTomita <d310151c2@gmail.com>
2024-06-17 15:02:16 +0900

    confirm react-jest.yaml is activate to run
    fix typo

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
+// 画面にLearn Reactの文字が出るかテスト
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
