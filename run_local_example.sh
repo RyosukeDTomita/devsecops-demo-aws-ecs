25bb9dea2ef398568c9b42f436b97797f7f17546
RyosukeDTomita <d310151c2@gmail.com>
2024-08-04 13:23:25 +0900

    fix jest
    fix jest error2
    
    test3
    
    test4
    
    test5
    
    test6
    
    test7
    
    test7
    
    update document
    
    document update2

--- run_local_example.sh
+++ run_local_example.sh
@@ -7,15 +7,18 @@
 # Author: Ryosuke Tomita
 # Date: 2024/08/01
 ##########################################################################
-#docker rmi react-app:latest -f
-#docker build -t react-app:latest . --no-cache
-#docker run -p 80:8080 react-app:latest # -p localport:containerport
+# -----composeを使わない場合-----
+# docker build -t react-app:latest . --build-arg BUILD_ENV=production
+# docker run -p 80:8080 react-app:latest # -p localport:containerport
 
+# -----composeを使う-----
 # .env.developmentでbuildxでbuild
 docker buildx bake --set react-app.args.BUILD_ENV=development
 # .env.productionでbuildxでbuild
 # docker buildx bake --set react-app.args.BUILD_ENV=production
-
+# buildxを使わない場合の引数の設定方法
+#docker compose build --build-arg BUILD_ENV=production
+# 起動
 docker compose up
 
 # open your browser and go to `localhost:80`.
